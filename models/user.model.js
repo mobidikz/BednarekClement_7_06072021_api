f1a9cf6737a31bbad40cddcb0c60b2a91e111524
Clément <clement.bednarek@gmail.com>
2021-07-24 18:36:06 +0200

    ajout image

--- models/user.model.js
+++ models/user.model.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema(
     },
     picture: {
         type: String,
-        default: "./uploads/profil/random-user.png"
+        default: "./uploads/profil/random-user.jpg"
     },
     bio: {
         type: String,
